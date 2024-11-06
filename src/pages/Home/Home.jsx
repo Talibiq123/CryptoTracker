3ee53e819fdc6ca07bc1cddf72e3b63882964ffd
Talibiq123 <talibsaeed90@gmail.com>
2024-11-06 21:47:29 +0530

    Crypto Table...

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,12 +5,21 @@ const Home = () => {
   return (
     <div className='home'>
       <div className='hero'>
-        <h1>Largest <br /> Crypto Meketplace</h1>
+        <h1>Largest <br /> Crypto Marketplace</h1>
         <p>Welcome to the World's Largest Cryptocurrency Marketplace. Sign up to explore more about cryptos.</p>
         <form action="">
           <input type="text" placeholder='Search crypto...' />
           <button type='submit'>Search</button>
         </form>
+      </div>
+      <div className='crypto-table'>
+        <div className="table-layout">
+          <p>#</p>
+          <p>Coins</p>
+          <p>Price</p>
+          <p>24H Changes</p>
+          <p>Market Cap</p>
+        </div>
       </div>
     </div>
   )
