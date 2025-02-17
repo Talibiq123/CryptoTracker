59c052fa9e3a4afd8df55f05a6b03a2e6c79c095
Talibiq123 <talibsaeed90@gmail.com>
2025-02-17 20:30:17 +0530

    Coin

--- src/pages/Coin/Coin.jsx
+++ src/pages/Coin/Coin.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './coin.css'
+import { useParams } from 'react-router-dom'
 
 const Coin = () => {
+
+  const { coinId } = useParams();
+  const [coinData, setCoinData] = useState();
+
+  const fetchCoinData = async () => {
+    // const url = 'https://api.coingecko.com/api/v3/coins/markets';
+    const options = { method: "GET", headers: { accept: "application/json" } };
+
+    fetch(url, options)
+      .then((res) => res.json())
+      .then((json) => console.log(json))
+      .catch((err) => console.error(err));
+  };
+
   return (
     <div>
-      
+      <h1>Coin : {coinId}</h1>
     </div>
   )
 }
